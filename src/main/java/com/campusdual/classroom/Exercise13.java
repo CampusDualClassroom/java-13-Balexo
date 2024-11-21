f71696561d42f6d4661038b05f09a5170d147587
Oscar Vallejo <oscar.vallejo@outlook.com>
2024-11-21 17:30:08 +0100

    Exercise 13 resuelto

--- src/main/java/com/campusdual/classroom/Exercise13.java
+++ src/main/java/com/campusdual/classroom/Exercise13.java
@@ -5,7 +5,8 @@ public class Exercise13 {
         FuelTank ft = new FuelTank();
         ft.showDetails();
         System.out.println("Actualización capacidad");
-        ft.actualFuel = -8;
+        System.out.println(ft.getActualFuel());
+        ft.setActualFuel(-8);
         ft.showDetails();
     }
 }
--- src/main/java/com/campusdual/classroom/FuelTank.java
+++ src/main/java/com/campusdual/classroom/FuelTank.java
@@ -2,13 +2,23 @@ package com.campusdual.classroom;
 
 public class FuelTank {
 
-  public int actualFuel = 10;
+
+  private int actualFuel = 10;
 
   public void showDetails() {
     System.out.println("La capacidad actual es de " + this.actualFuel + " litros.");
   }
 
   public int getActualFuel() {
+
     return this.actualFuel;
+  }
+
+  public void setActualFuel(int actualFuel) {
+    if (actualFuel < 0) {
+      System.out.println("No puede ser negativa");
+    } else {
+      this.actualFuel = actualFuel;
+    }
   }
 }
